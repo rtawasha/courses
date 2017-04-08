7fbcb271aafb325c68a9a5738744b8f918227a83
rtawasha <rtawasha@users.noreply.github.com>
2017-04-08 00:37:30 -0700

    Update setup_p2.sh

--- setup/setup_p2.sh
+++ setup/setup_p2.sh
@@ -9,9 +9,9 @@ if [ $region = "us-west-2" ]; then
 elif [ $region = "eu-west-1" ]; then
    export ami="ami-b43d1ec7" # Ireland
 elif [ $region = "us-east-1" ]; then
-  export ami="ami-31ecfb26" # Virginia
+   export ami="ami-31ecfb26" # Virginia
 else
-  echo "Only us-west-2 (Oregon), eu-west-1 (Ireland), and us-east-1 (Virginia) are currently supported"
+   echo "Only us-west-2 (Oregon), eu-west-1 (Ireland), and us-east-1 (Virginia) are currently supported"
   exit 1
 fi
 
